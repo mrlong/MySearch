f7fbafd6fe386e19efd37f5b0cfbf0fba710730f
mrlong <mrlong.com@gmail.com>
2013-09-29 18:12:13 +0800

    修改:   1.又能一次结构变化,增加controllers

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
+app.use(flash());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,34 @@
  * GET home page.
  */
 
-
+var user=require('../controllers/user.con');
 
 module.exports = function(app) {
   
-  app.get('/', function (req, res) {
+  //认证
+  var auth;
+  auth = function(req, res, next) {
+    if (req.cookies.user) {
+      return next();
+    } 
+    else {
+      if (req.session.user) {
+        return next();
+      } 
+      else {
+        return res.redirect('/login');
+      }
+    }
+  };
+
+  app.get('/',function (req, res) {
     res.render('index', { title: 'Express',url:'/',mrlong:'33333377766' });
-    return false;
   });
 
-  app.get('/login',function (req,res){
-    res.render('login', { title: 'Express',url:'/login',mrlong:'ssss' });
-    return false;
+  app.get('/login',user.login);
+
+  app.get('/vip',auth,function (req,res){
+
   });
 
   app.use(function (req, res) {
