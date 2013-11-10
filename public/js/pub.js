59d1722a933aeac73e32d8af3b23033bb533a770
龙仕云 <mrlong.com@gmail.com>
2013-11-10 18:08:09 +0800

    增加保存的提示。

--- public/js/pub.js
+++ public/js/pub.js
@@ -19,6 +19,46 @@
 
   });
 
+
+
+  //在提示成功，或失败窗口。
+  // $(".panel").msgbox(data.msg);
+  // $(".panel").msgbox(data.msg,false);
+  $.fn.msgbox=function(msg,success){
+    var jpanel = $(this).parents('.panel');
+
+    //没有则直接提示窗口了。
+    if(!jpanel){
+      alert(msg);
+      return false;
+    };
+
+    jpanel.find("#yunzjmsgbox").remove();
+
+    var jtxt = $('<div class="alert " id="yunzjmsgbox">'+
+      '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+
+      '<span class="glyphicon"></span></div>');
+    
+    if (success==undefined || success==true){
+      jtxt.addClass('alert-success');
+      jtxt.find('span').addClass('glyphicon-ok');
+      jtxt.find('span').after(' '+msg);
+    }
+    else{
+      jtxt.addClass('alert-danger');
+      jtxt.find('span').addClass('glyphicon-remove');
+      jtxt.find('span').after(' '+msg);
+    };
+    jpanel.after(jtxt);
+
+    //启动定时关闭提示窗口
+    setTimeout(function(){
+      $("#yunzjmsgbox").remove();
+    },2000);
+  };  
+
+
+ 
   
 }(window.jQuery)
 
